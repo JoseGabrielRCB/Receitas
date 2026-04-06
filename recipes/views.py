afce2fc669d38fda6546d76f92993c9b23da67f4
Jose Gabriel Barbosa <pidgecapibaribe@gmail.com>
2026-04-06 06:45:59 -0400

    Adcionando fonte alwsome

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http  import HttpResponse
  
 def home(request):
-    return render(request,'/recipes/home.html',
+    return render(request,'recipes/home.html',
                   status=201,
                   context={'name' : 'Jose gabriel'})
