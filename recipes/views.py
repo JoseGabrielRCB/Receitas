7071f64281d8c2fdcdfc579d5fb40e445596d0e0
Jose Gabriel Barbosa <pidgecapibaribe@gmail.com>
2026-04-05 20:37:35 -0400

    Criando Variaveis no Template DJango

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http  import HttpResponse
  
 def home(request):
-    return render(request,'recipes/home.html')
+    return render(request,'./recipes/home.html',
+                  status=201,
+                  context={'name' : 'Luiz Otávio'})
 
 def contato(request):
     return HttpResponse("CONTATO")
