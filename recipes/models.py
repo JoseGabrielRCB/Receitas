71f6a3063086185b72f624c6320dd19a6eaf472f
JoseGabrielRCB <pidgecapibaribe@gmail.com>
2026-05-07 10:53:52 -0400

    Migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,21 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-# EDITED
-# title description slug
-# preparation_time preparation_time_unit
-# servings servings_unit
-# preparation_step
-# preparation_step_is_html
-# created_at updated_at
-# is_published
-# cover
-# category (Relação)
-# Author (Relação)
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
 
 class Recipe(models.Model):
-    title = models.models.CharField(max_length=50)
+    #Campos Base
+    title = models.CharField(max_length=50)
     slug = models.SlugField()
     descipition = models.CharField(max_length=1000)
     serving = models.CharField( max_length=50)
@@ -28,4 +25,7 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/') #salvar o local em data especifica
-    
+
+    # Relacoes
+    Category= models.ForeignKey(Category,on_delete=models.SET_NULL,null=True) # Caso seja deletado, seta como NULL
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null = True)
